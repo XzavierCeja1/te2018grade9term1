97f41ee28313a82bfb91b0608830a4806d0b5dde
XzavierCeja1 <Xzavier@csegde.org>
2018-10-06 13:19:05 -0700

    Updated Data.Js

--- data.js
+++ data.js
@@ -1,5 +1,13 @@
 // Example
-let message = "Hello World!";
+let message = "Hello Xzavier!";
 console.log(message);
+let bDayMonth=9
+let age=14
+let siblings=1
+let people = "Help me"
+let sound = "loud"
+let fortnightgood = false
+let meCold = false
+let meHaveNoCatchFrase = true
 
 // Type your code below!
